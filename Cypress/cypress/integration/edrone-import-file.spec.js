c3a5f26c7f0c5237b44c5b1c1b5e3ee251878fdc
Kamil Kuczynski <kamil.kuczynski@avenga.com>
2021-02-08 18:28:45 +0100

    cypress

---
+++ Cypress/cypress/integration/edrone-import-file.spec.js
@@ -0,0 +1,40 @@
+describe('Login', () => {
+    beforeEach(() => {
+        cy.loginToMissionControl();
+      })
+
+    it('Upload users data ', () => {
+        const fileName = Cypress.config().csvFileName;
+
+        cy.visit(Cypress.config().baseUrl + "settings/import");
+        cy.importUsers(fileName, Cypress.config().csvFilePath + fileName);
+    }) 
+
+    it('User have a birth date', () => {
+        const user = 'edrone1%40test.pl';
+        cy.visit(Cypress.config().baseUrl + 'customer/single?email=' + user);
+
+        cy.wait(5000);
+        cy.get('.personal-details > div > div > p').should('contain', '2020-4-4');
+    }) 
+
+    it('User do not have a birth date', () => {
+        const user = 'edrone2%40test.pl';
+        cy.visit(Cypress.config().baseUrl + 'customer/single?email=' + user);
+
+        cy.wait(5000);
+        cy.get('.personal-details > div > div > p').should('not.contain', '1986');
+    }) 
+
+    it('Update user to add birth date', () => {
+        const fileName = Cypress.config().csvUpdateFileName;
+        const user = 'edrone3%40test.pl';
+
+        cy.visit(Cypress.config().baseUrl + "settings/import");
+        cy.importUsers(fileName, Cypress.config().csvFilePath + fileName);
+        cy.visit(Cypress.config().baseUrl + 'customer/single?email=' + user);
+
+        cy.wait(5000);
+        cy.get('.personal-details > div > div > p').should('contain', '1986-2-1');
+    }) 
+})
---
+++ Cypress/cypress/support/commands.js
@@ -0,0 +1,27 @@
+import 'cypress-file-upload';
+
+Cypress.Commands.add('loginToMissionControl', () => {
+    cy.visit(Cypress.config().baseUrl + "login");
+    cy.get('#login__username').type(Cypress.config().login);
+    cy.get('#login__password').type(Cypress.config().pass);
+    cy.get('.btn-login').click();
+    cy.wait(30000);
+    cy.get('.user-name > p').should('include.text', 'Kamil');
+})
+
+Cypress.Commands.add('importUsers', (fileName, filePath) => {
+    cy.readFile(filePath).then(function (fileContent) {
+
+        cy.get('#file').attachFile({ fileContent, fileName, mimeType: 'application/csv' })
+    
+        cy.xpath('//tr[1]/td[2]/select[@class="form-control"]').select('EMAIL');
+        cy.on('uncaught:exception', (err, runnable) => {
+            expect(err.message).to.include('The following error originated from your application code, not from Cypress')
+            done()
+            return false
+          });
+        cy.xpath('//tr[2]/td[2]/select[@class="form-control"]').select('BIRTH_DATE (YYYY-MM-DD)');
+        cy.xpath('//div[13]/div/section/div[1]/div[5]/div/div[3]/div/div/div/a').click();
+        cy.get('h3').should('contain', 'Your file is being processed. We will inform you by email when it is completed');
+    })
+})
